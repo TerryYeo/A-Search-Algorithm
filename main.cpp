168883105ea3e2df5eb5622b1f7fbd7b4278367d
YOUR NAME <yyshien94@gmail.com>
2015-12-16 08:59:37 +0800

    first step

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <list>
 using namespace std;
 
 struct point{
@@ -7,10 +8,28 @@ struct point{
 	int y = 0;
 };
 
+int g(){
+	return (this.x + this.y);
+}
+
+int h(){
+	return 0;
+}
+
+int f(){
+	return this.g() + this.h();
+}
+
 int main(){
-	//dsaaaaaaaaaaaaaaaaaaaaaa
-	int b = 5;
-	int Terry = 1;
-	int a = 9;
+	//define two list
+	list<point> openList;
+	list<point> closeList;
+
+	 //print the close list result out for showing professor
+	cout << "The contents of way are: ";
+	for (list<point>::iterator it = closeList.begin(); it != closeList.end(); it++)
+    	std::cout << '( << *it.x << ',' << *it.y << ") ";
+
+  	std::cout << '\n';
 	return 0;
 }
