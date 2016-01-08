b6d5b6f0d512e18b8611d36cbe13b7fd9b67d4ce
SungJun-hong <tjdwnsdl8@naver.com>
2016-01-08 12:21:59 +0100

    Now in working 12:21pm Jan.8th
    I dont't know how to implement function outside.(ex. print(),
    searchAround function())
    I tried but I got error T-T

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 //define a new class Point
+
+
 class Point{
 	private:
 		int x = 0;
@@ -25,7 +27,8 @@ class Point{
 		void setG() {; }
 		void setH() { h = (4 - x) + (4 - y); }
 
-		void setBlocked() { blocked = true; }
+		void setBlocked() { blocked = true; ;
+		}
 		void setPassed() { passed = true; }
 		void unSetPassed() { passed = false; }
 
@@ -57,8 +60,8 @@ class Point{
 		Point *getP() { return p; }
 		//int getPY(Point p) { return py; }
 
-		bool getBlocked() { return blocked; }
-		bool getPassed() { return passed; }
+		bool isBlocked() { return blocked; }
+		bool isPassed() { return passed; }
 
 
 };
@@ -80,45 +83,52 @@ Point min_p(list<Point> l){
 }
 
 //void print(Point);
-
+void searchAround(int, int);
 
 int main(){
 
 	//define two list
 	list<Point> openList;
 	list<Point> closeList;
-	Point map[5][5];
+	
+	Point map[7][7];
 
 	int nowX = 0;
-	int nowY = 0;
+	int nowY = 0;	
 	
-	
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 7; j++) {
 			map[i][j].setPoint(i, j);
 			//map[i][j].setParent(0, 0);
 			map[i][j].setH();
 		}
 	}
-	openList.push_back(map[0][0]);     //   S 0 0 0 0
-	closeList.push_back(map[1][0]);    //   1 1 0 1 1
-	closeList.push_back(map[1][1]);    //   0 0 0 0 0
-	closeList.push_back(map[1][3]);    //   0 1 0 1 1
-	closeList.push_back(map[1][4]);    //   0 1 0 0 E
-	closeList.push_back(map[3][1]);
-	closeList.push_back(map[3][3]);
-	closeList.push_back(map[3][4]);
-	closeList.push_back(map[4][1]);
-	map[1][0].setBlocked();
-	map[1][1].setBlocked();
-	map[1][3].setBlocked();
-	map[1][4].setBlocked();
-	map[3][1].setBlocked();
-	map[3][3].setBlocked();
-	map[3][4].setBlocked();
-	map[4][1].setBlocked();
-
-
+	openList.push_back(map[1][1]);      // X X X X X X X
+	closeList.push_back(map[2][1]);     // X S 0 0 0 0 X
+	closeList.push_back(map[2][2]);     // X 1 1 0 1 1 X
+	closeList.push_back(map[2][4]);		// X 0 0 0 0 0 X
+	closeList.push_back(map[2][5]);		// X 0 1 0 1 1 X
+	closeList.push_back(map[4][2]);		// X 0 1 0 0 E X
+	closeList.push_back(map[4][4]);		// X X X X X X X
+	closeList.push_back(map[4][5]);
+	closeList.push_back(map[5][2]);
+	map[2][1].setBlocked();
+	map[2][2].setBlocked();
+	map[2][4].setBlocked();
+	map[2][5].setBlocked();
+	map[4][2].setBlocked();
+	map[4][4].setBlocked();
+	map[4][5].setBlocked();
+	map[5][2].setBlocked();
+	////////////////////////////Make Outside Blocked
+	for (int i; i < 7; i++) {
+		for (int j; j < 7; j++) {
+			if (i == 0 || i == 6 || j == 0 || j == 6) {
+				map[i][j].setBlocked();
+				closeList.push_back(map[i][j]);
+			}
+		}
+	}
 	
 	while ((openList.empty()) || (nowX == 4 && nowY == 4)) {
 		
@@ -126,27 +136,49 @@ int main(){
 		//add the minimun f() element into closeList
 		closeList.push_back(tempPoint);
 		//delete the element from openList	
+		openList.remove(tempPoint);
+		nowX = tempPoint.getX();
+		nowY = tempPoint.getY();
+		//searchAround(nowX, nowY);
+
+		//searchAround
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (map[nowX +i][nowY + j].isBlocked()==false || map[nowX + i][nowY + j].isPassed() ==false) {
+
+
+				}
+			}
+		}
+
+
 
   		cout << '\n';
   	}
 	
-	////////////////////////////////////////////////////////////////////////print
+	////Print
 	cout << "Map : \n";
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
-			if (map[i][j].getBlocked()) { cout << "x "; continue; }
-			if (map[i][j].getPassed()) { cout << "o "; continue; }
+			if (map[i][j].isBlocked()) { cout << "x "; continue; }
+			if (map[i][j].isPassed()) { cout << "o "; continue; }
 			cout << "- ";
 
 		}
 		cout << "\n";
 	}
-	////////////////////////////////////////////////////////////////////////
+	////
 
 
 	return 0;
 }
 
+/*
+void searchAround(int x, int y) {
+	Point temp =(Point)map[x-1][y-1]
+}
+*/
+//failed to implement independantly. T-T
 /*
 void print(Point* map) {
 	cout << "Map : \n";
