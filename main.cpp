aa67a870edb057d0edcb8b14a2e4893bcd1a4f43
SungJun-hong <tjdwnsdl8@naver.com>
2016-01-08 14:25:32 +0100

    I don't know how to erase from the list.
    I don't know how to erase from the list.
    and I changed maze a little!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 
 #include <iostream>
 #include <list>
+#include <stdlib.h>
+#include <windows.h>
 using namespace std;
 
 //define a new class Point
@@ -105,8 +107,8 @@ int main(){
 	}
 	openList.push_back(map[1][1]);      // X X X X X X X
 	closeList.push_back(map[2][1]);     // X S 0 0 0 0 X
-	closeList.push_back(map[2][2]);     // X 1 1 0 1 1 X
-	closeList.push_back(map[2][4]);		// X 0 0 0 0 0 X
+	closeList.push_back(map[2][2]);     // X 1 1 1 0 1 X
+	closeList.push_back(map[2][3]);		// X 0 0 0 0 0 X
 	closeList.push_back(map[2][5]);		// X 0 1 0 1 1 X
 	closeList.push_back(map[4][2]);		// X 0 1 0 0 E X
 	closeList.push_back(map[4][4]);		// X X X X X X X
@@ -114,15 +116,15 @@ int main(){
 	closeList.push_back(map[5][2]);
 	map[2][1].setBlocked();
 	map[2][2].setBlocked();
-	map[2][4].setBlocked();
+	map[2][3].setBlocked();
 	map[2][5].setBlocked();
 	map[4][2].setBlocked();
 	map[4][4].setBlocked();
 	map[4][5].setBlocked();
 	map[5][2].setBlocked();
 	////////////////////////////Make Outside Blocked
-	for (int i; i < 7; i++) {
-		for (int j; j < 7; j++) {
+	for (int i =0; i < 7; i++) {
+		for (int j= 0; j < 7; j++) {
 			if (i == 0 || i == 6 || j == 0 || j == 6) {
 				map[i][j].setBlocked();
 				closeList.push_back(map[i][j]);
@@ -130,55 +132,80 @@ int main(){
 		}
 	}
 	
-	while ((openList.empty()) || (nowX == 4 && nowY == 4)) {
+	while ((openList.empty()) || !(nowX == 4 && nowY == 4)) {
 		
 		Point tempPoint = min_p(openList);
 		//add the minimun f() element into closeList
-		closeList.push_back(tempPoint);
+		
+		int absX = abs(tempPoint.getX() - nowX);
+		int absY = abs(tempPoint.getY() - nowY);
+
+		map[nowX][nowY].setPassed();
+
+		if (absX >1 && absY >1) {
+			nowX = (*(map[nowX][nowY].getP())).getX();
+			nowY = (*(map[nowX][nowY].getP())).getY();
+		}
+
+		closeList.push_back(map[tempPoint.getX()][tempPoint.getY()]);
+		map[tempPoint.getX()][tempPoint.getY()].setBlocked();
+		
 		//delete the element from openList	
-		openList.remove(tempPoint);
+		//openList.erase(map[tempPoint.getX()][tempPoint.getY()]);
 		nowX = tempPoint.getX();
 		nowY = tempPoint.getY();
 		//searchAround(nowX, nowY);
 
-		//searchAround
+
+		//searchAround and add to openList
 		for (int i = -1; i < 2; i++) {
 			for (int j = -1; j < 2; j++) {
 				if (map[nowX +i][nowY + j].isBlocked()==false || map[nowX + i][nowY + j].isPassed() ==false) {
-
-
+					openList.push_back(map[nowX + i][nowY + i]);
 				}
 			}
 		}
 
 
+		////Print
+		cout << "Map : \n";
+		for (int i = 1; i < 6; i++) {
+			for (int j = 1; j < 6; j++) {
+				if (nowX == i && nowY == j) { cout << "@ "; continue; }
+				if (map[i][j].isBlocked()) { cout << "x "; continue; }
+				if (map[i][j].isPassed()) { cout << "o "; continue; }
+				
+				cout << "- ";
 
-  		cout << '\n';
-  	}
-	
-	////Print
-	cout << "Map : \n";
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			if (map[i][j].isBlocked()) { cout << "x "; continue; }
-			if (map[i][j].isPassed()) { cout << "o "; continue; }
-			cout << "- ";
-
+			}
+			cout << "\n";
 		}
-		cout << "\n";
-	}
-	////
 
+		for (int i = 1; i < 6; i++) {
+			for (int j = 1; j < 6; j++) {
+				cout << map[i][j].isPassed() << " ";
+
+
+			}
+			cout << "\n";
+		}
+
+		////
+  		cout << '\n';
+		Sleep(2000);
+
+  	}
 
 	return 0;
 }
 
+//failed to implement outside of main() independantly. T-T
 /*
 void searchAround(int x, int y) {
 	Point temp =(Point)map[x-1][y-1]
 }
 */
-//failed to implement independantly. T-T
+//failed to implement outside of main() independantly. T-T
 /*
 void print(Point* map) {
 	cout << "Map : \n";
